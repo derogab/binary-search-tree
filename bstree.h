ddc3a831798f603c4b1a6cf447f812197341f7b2
Gabriele De Rosa <derosagabriele@outlook.it>
2020-03-29 16:51:07 +0200

    Let's start

--- bstree.h
+++ bstree.h
@@ -1,6 +1,79 @@
 #ifndef BSTREE_H
 #define BSTREE_H
 
+/**
+ * Classe generica che implementa un albero binario di ricerca.
+ * 
+ * @brief Albero binario di ricerca
+ * 
+ * @param T tipo del dato
+*/
+template <typename T>
+class binary_search_tree {
 
+private:
+    
+    /**
+     * Struttura di supporto interna che implementa un nodo dell'albero
+     * 
+     * @brief Nodo dell'albero
+    */
+    struct node {
+        
+        T value; // valore del dato inserito
+        node *left; // figlio sinistro del nodo
+        node *right; // figlio destro del nodo
+
+        /**
+         * Costruttore di default
+        */
+        node(): left(nullptr), right(nullptr) {}
+
+        /**
+         * Costruttore secondario che inizializza il nodo
+         * @param v valore del dato
+        */
+        node(const T &v): value(v), left(nullptr), right(nullptr) {}
+
+        /**
+         * Costruttore secondario che inizializza il nodo
+         * @param v valore del dato
+         * @param n puntatore al figlio sinistro
+        */
+        node(const T &v, node *n): value(v), left(n), right(nullptr) {}
+        
+        /**
+         * Costruttore secondario che inizializza il nodo
+         * @param v valore del dato
+         * @param n1 puntatore al figlio sinistro
+         * @param n2 puntatore al figlio destro
+        */
+        node(const T &v, node *n1, node *n2): value(v), left(n1), right(n2) {}
+
+        /**
+         * Distruttore
+        */
+        ~node() {
+            left = nullptr;
+            right = nullptr;
+        }
+
+    };
+
+    node *_root; // puntatore alla radice dell'albero    
+
+public:
+    
+    /**
+     * Costruttore di default 
+    */
+    binary_search_tree(): _root(nullptr) {}
+
+    /**
+     * Distruttore
+    */
+    ~binary_search_tree(){}
+
+};
 
 #endif
---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include "bstree.h"
+
+
+int main(int argc, char const *argv[])
+{
+    binary_search_tree<int> tree;
+
+    return 0;
+}
