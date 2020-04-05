4a730983c831ed35c8ab8491c1f1197208585cfc
Gabriele De Rosa <derosagabriele@outlook.it>
2020-04-06 01:28:16 +0200

    Tests

--- main.cpp
+++ main.cpp
@@ -3,6 +3,18 @@
 
 
 /**
+ * Struct point che implementa un punto 2D.
+ * 
+ * @brief Struct point che implementa un punto 2D.
+*/
+struct point {
+    int x; ///< coordinata x del punto
+    int y; ///< coordinata y del punto
+
+    point(int xx, int yy) : x(xx), y(yy) {}
+};
+
+/**
  * Funtore per l'uguaglianza tra interi.
  * 
  * @brief Funtore per l'uguaglianza tra interi.
@@ -49,6 +61,39 @@ struct compare_string {
 };
 
 /**
+ * Funtore per il confronto di uguaglianza tra due punti.
+ * Ritorna true se p1.x != p2.x.
+ * 
+ * @brief Funtore per il confronto di due punti.
+*/
+struct equal_point {
+    bool operator()(const point &p1, const point &p2) const {
+        return (p1.x==p2.x) && (p1.y==p2.y);
+    } 
+};
+
+/**
+ * Funtore per il confronto di due punti. 
+ * Ritorna true se p1.x < p2.x.
+ * 
+ * @brief Funtore per il confronto di due punti.
+*/
+struct compare_point {
+    bool operator()(const point &p1, const point &p2) const {
+        return (p1.x<p2.x);
+    } 
+};
+
+/**
+ * Ridefinizione dell'operatore di stream << per un point.
+ * Necessario per l'operatore di stream della classe binary_search_tree.
+*/
+std::ostream &operator<<(std::ostream &os, const point &p) {
+	std::cout << "(" << p.x << "," << p.y << ")";
+	return os;
+}
+
+/**
  * Predicato di cofronto del valore con 3.
  * La valutazione è fatta sul numero intero.
  * Ritorna true se il valore è maggiore di 3.
@@ -56,90 +101,154 @@ struct compare_string {
  * @brief Predicato per il confronto dei valori.
 */
 struct is_plus_than_3 {
-  bool operator()(int value) const {
-    return value > 3;
-  }
+    bool operator()(int value) const {
+        return value > 3;
+    }
 };
+
+void test_metodi_fondamentali() {
+    std::cout << "******** Test sui metodi fondamentali ********" << std::endl;
+
+	binary_search_tree<int, compare_int, equal_int> bstree;
+
+	bstree.add(1);
+	bstree.add(2);
+	bstree.add(0);
+
+	std::cout << "stampa di bstree dopo inserimenti" << std::endl << bstree << std::endl;
+
+	binary_search_tree<int, compare_int, equal_int> bstree2(bstree);
+
+	std::cout << "stampa di bstree2" << std::endl << bstree2 << std::endl;
+
+	binary_search_tree<int, compare_int, equal_int> bstree3;
+
+	bstree3 = bstree;	
+
+	std::cout << "stampa di bstree3" << std::endl << bstree3 << std::endl;
+}
+
+/**
+ * Test sull'albero di interi
+*/
+void test_tree_int(binary_search_tree<int, compare_int, equal_int> &other) {
+    std::cout << "******** Test sull'albero di interi ********" << std::endl;
+
+    std::cout << "Insertimento dei valori 6, 2, 7, 1, 4, 9, 3, 5, 8" << std::endl;
+    other.add(6);
+    other.add(2);
+    other.add(7);
+    other.add(1);
+    other.add(4);
+    other.add(9);
+    other.add(3);
+    other.add(5);
+    other.add(8);
+
+    std::cout << "Stampa con operator<<" << std::endl;
+    std::cout << other << std::endl;
+
+    std::cout<< "Dimensione dell'albero: " << other.size() << std::endl;
+
+    std::cout << "Stampa con iteratori: " << std::endl;
+
+    binary_search_tree<int, compare_int, equal_int>::const_iterator i,ie;
+
+    for(i=other.begin(),ie=other.end(); i!=ie; ++i)
+        std::cout << *i << std::endl;
+
+    other.clear();
+    std::cout << "Dimensione dell'albero dopo clear(): " << other.size() << std::endl;
+}
+
+void test_const_tree_int(const binary_search_tree<int, compare_int, equal_int> &other) {
+    std::cout << "******** Test sull'albero costante di interi ********" << std::endl;
+
+    // add non è chiamabile su un oggetto const
+
+    std::cout << "Stampa con operator<<" << std::endl;
+    std::cout << other << std::endl;
+
+    std::cout << "Dimensione dell'albero: " << other.size() << std::endl;
+
+    std::cout << "Stampa con iteratori" << std::endl;
+
+    binary_search_tree<int, compare_int, equal_int>::const_iterator i,ie;
+
+    for(i=other.begin(),ie=other.end(); i!=ie; ++i)
+        std::cout<<*i<<std::endl;
+
+    // clear non è chiamabile su un oggetto const
+}
+
+void test_funtore_stringhe(void) {
+	std::cout << "******** Test sull'albero di stringhe ********" << std::endl;
+
+	binary_search_tree<std::string, compare_string,equal_string> str_tree;
+
+	std::cout << "Insertimento dei valori 'pippo', 'pluto', 'paperino', 'cip'" << std::endl;
+	str_tree.add("pippo");
+	str_tree.add("pluto");	
+	str_tree.add("paperino");
+	str_tree.add("cip");
+
+	std::cout << "Stampa con operator<<" << std::endl;
+	std::cout << str_tree << std::endl;
+
+	std::cout << "Dimensione dell'albero: " << str_tree.size() << std::endl;
+
+	std::cout << "Ricerca di 'cip': " << str_tree.find("cip") << std::endl;
+	std::cout << "Ricerca di 'cipp': " << str_tree.find("cipp") << std::endl;
+
+	//E' possibile usare le asserzioni per verificare dei test
+	//assert(str_tree.find("cip")==true);
+}
+
+void test_tree_point(void) {
+
+	binary_search_tree<point, compare_point, equal_point> point_tree;
+
+	std::cout << "******** Test sull'albero di point ********" << std::endl;
+
+	std::cout << "Insertimento dei valori (1,1), (1,2), (2,7), (0,0), (5,4)" << std::endl;
+	point_tree.add(point(1,1));
+	point_tree.add(point(1,2));
+	point_tree.add(point(2,7));
+	point_tree.add(point(0,0));
+	point_tree.add(point(5,4));
+
+	std::cout << "Stampa con operator<<" << std::endl;
+	std::cout << point_tree << std::endl;
+
+	std::cout << "Dimensione dell'albero: " << point_tree.size() << std::endl;
+
+	std::cout << "Stampa con iteratori" << std::endl;
+
+	binary_search_tree<point,compare_point,equal_point>::const_iterator i,ie;
+
+	for(i=point_tree.begin(),ie=point_tree.end(); i!=ie; ++i)
+		std::cout<<*i<<std::endl;
+
+	std::cout << "Ricerca di '(1,1)': " << point_tree.find(point(1,1)) << std::endl;
+	std::cout << "Ricerca di '(2,2)': " << point_tree.find(point(2,2)) << std::endl;
+
+}
 
 /**
  * Funzione MAIN con i vari test.
 */
 int main(int argc, char const *argv[]) {
 
-    binary_search_tree<int, compare_int, equal_int> int_tree;
-    binary_search_tree<std::string, compare_string, equal_string> string_tree;
+    // variabili 
+    binary_search_tree<int, compare_int, equal_int> int_test_tree;
 
-    // inserimenti interi
-    int_tree.add(6);
-    int_tree.add(2);
-    int_tree.add(7);
-    int_tree.add(1);
-    int_tree.add(4);
-    int_tree.add(9);
-    int_tree.add(3);
-    int_tree.add(5);
-    int_tree.add(8);
+    // tests 
+    test_metodi_fondamentali();
+    test_tree_int(int_test_tree);
+    test_const_tree_int(int_test_tree);
+    test_funtore_stringhe();
+    test_tree_point();
 
-    // inserimenti stringhe
-    string_tree.add("ab");
-    string_tree.add("a");
-    string_tree.add("abc");
-    string_tree.add("abcd");
-    string_tree.add("abcde");
-
-    // cerca interi
-    std::cout << "int_tree.find(5): " << int_tree.find(5) << std::endl;
-    std::cout << "int_tree.find(6): " << int_tree.find(6) << std::endl;
-
-    // cerca stringhe
-    std::cout << "string_tree.find(\"abcde\"): " << string_tree.find("abcde") << std::endl;
-    std::cout << "string_tree.find(\"abcdef\"): " << string_tree.find("abcdef") << std::endl;
-
-    // get size
-    std::cout << "int_tree.size(): " << int_tree.size() << std::endl;
-    std::cout << "string_tree.size(): " << string_tree.size() << std::endl;
-
-    // test iterator
-    typename binary_search_tree<int, compare_int, equal_int>::const_iterator i,ie;
-
-	i = int_tree.begin();
-	ie = int_tree.end();
-
-    std::cout << "string_tree: " << string_tree << std::endl;
-
-    binary_search_tree<int, compare_int, equal_int> int_tree_copy(int_tree);
-    binary_search_tree<int, compare_int, equal_int> int_tree_pnt;
-
-    int_tree_pnt = int_tree;
-
-    std::cout << "int_tree: " << int_tree << std::endl;
-    std::cout << "int_tree_pnt: " << int_tree_pnt << std::endl;
-    std::cout << "int_tree_copy: " << int_tree_copy << std::endl;
-
-    int_tree.add(10);
-    int_tree.add(100);
-    int_tree_pnt.add(11);
-    int_tree_copy.add(12);
-    int_tree_copy.add(13);
-    int_tree_copy.add(14);
-    int_tree_copy.add(15);
-
-    std::cout << "int_tree: " << int_tree << std::endl;
-    std::cout << "int_tree_pnt: " << int_tree_pnt << std::endl;
-    std::cout << "int_tree_copy: " << int_tree_copy << std::endl;
-
-    std::cout << "int_tree.size(): " << int_tree.size() << std::endl;
-    std::cout << "int_tree_pnt.size(): " << int_tree_pnt.size() << std::endl;
-    std::cout << "int_tree_copy.size(): " << int_tree_copy.size() << std::endl;
-
-    binary_search_tree<int, compare_int, equal_int> int_subtree = int_tree.subtree(4);
-
-    std::cout << "int_subtree: " << int_subtree << std::endl;
-    std::cout << "int_subtree.size(): " << int_subtree.size() << std::endl;
-
-    is_plus_than_3 ipt3;
-    printIF(int_subtree, ipt3);
-    
     return 0;
 
 }
